0882194d49f8b5861290425f6bfcf82b000def6c
Ilvan <ilvanlopesjardim110@gmail.com>
2025-02-20 00:45:56 -0300

    feat: initial commit

---
+++ media-collection/src/main/java/com/ilvan/media_collection/MediaCollectionApplication.java
@@ -0,0 +1,13 @@
+package com.ilvan.media_collection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaCollectionApplication.class, args);
+	}
+
+}
---
+++ media-collection/src/test/java/com/ilvan/media_collection/MediaCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilvan.media_collection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaCollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
