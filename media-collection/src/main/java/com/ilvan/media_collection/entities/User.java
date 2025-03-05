82a9db8f0791950ace7dc868adb361824e4461a8
Ilvan <ilvanlopesjardim110@gmail.com>
2025-03-04 21:36:03 -0300

    feat: create entity user

---
+++ media-collection/src/main/java/com/ilvan/media_collection/entities/User.java
@@ -0,0 +1,27 @@
+package com.ilvan.media_collection.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String email;
+
+    private String password;
+
+}
