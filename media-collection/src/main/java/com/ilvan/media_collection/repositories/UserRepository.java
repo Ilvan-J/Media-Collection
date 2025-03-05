9b17b861010d9cd46e3ca8ccaa4b20a313a497e1
Ilvan <ilvanlopesjardim110@gmail.com>
2025-03-04 21:36:46 -0300

    feat: create user repository

---
+++ media-collection/src/main/java/com/ilvan/media_collection/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.ilvan.media_collection.repositories;
+
+import com.ilvan.media_collection.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
