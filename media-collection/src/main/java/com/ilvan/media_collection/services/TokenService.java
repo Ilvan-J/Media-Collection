0a7ade933377bc0f72c6295486670af51119fdf5
Ilvan <ilvanlopesjardim110@gmail.com>
2025-03-11 22:06:02 -0300

    feat: user and token services

---
+++ media-collection/src/main/java/com/ilvan/media_collection/services/TokenService.java
@@ -0,0 +1,54 @@
+package com.ilvan.media_collection.services;
+
+import com.ilvan.media_collection.controller.dto.LoginRequest;
+import com.ilvan.media_collection.controller.dto.LoginResponse;
+import com.ilvan.media_collection.controller.erros.CustomGenericException;
+import com.ilvan.media_collection.repositories.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+
+@Service
+public class TokenService {
+    private final JwtEncoder jwtEncoder;
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public TokenService(JwtEncoder jwtEncoder,
+                        UserRepository userRepository,
+                        BCryptPasswordEncoder passwordEncoder) {
+        this.jwtEncoder = jwtEncoder;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Transactional
+    public LoginResponse login(LoginRequest loginRequest) {
+        var user = userRepository.findByEmail(loginRequest.email());
+
+        if (user.isEmpty() || !user.get().isLoginCorrect(loginRequest, passwordEncoder)) {
+            throw new CustomGenericException("email or password is invalid!", HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+
+        var now = Instant.now();
+        var expiresIn = 300L;
+
+        var claims = JwtClaimsSet.builder()
+                .issuer("mybackend")
+                .subject(user.get().getUserId().toString())
+                .issuedAt(now)
+                .expiresAt(now.plusSeconds(expiresIn))
+                .build();
+
+        var jwtValue = jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+
+        return new LoginResponse(jwtValue, expiresIn);
+    }
+}
---
+++ media-collection/src/main/java/com/ilvan/media_collection/services/UserService.java
@@ -0,0 +1,49 @@
+package com.ilvan.media_collection.services;
+
+
+import com.ilvan.media_collection.controller.dto.CreateUserDto;
+import com.ilvan.media_collection.controller.erros.CustomGenericException;
+import com.ilvan.media_collection.entities.Role;
+import com.ilvan.media_collection.entities.User;
+import com.ilvan.media_collection.repositories.RoleRepository;
+import com.ilvan.media_collection.repositories.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Set;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository,
+                       RoleRepository roleRepository,
+                       BCryptPasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Transactional
+    public void createUser(CreateUserDto dto) {
+        var basicRole = roleRepository.findByName(Role.Values.BASIC.name());
+        var userFromDb = userRepository.findByEmail(dto.email());
+
+        if (userFromDb.isPresent()) {
+            throw new CustomGenericException("Email alredy in use", HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+
+        var user = new User();
+        user.setEmail(dto.email());
+        user.setPassword(passwordEncoder.encode(dto.password()));
+        user.setRoles(Set.of(basicRole));
+
+        userRepository.save(user);
+    }
+}
