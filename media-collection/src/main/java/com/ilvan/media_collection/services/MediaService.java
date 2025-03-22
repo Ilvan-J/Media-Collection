e0c77f1a1e53289032e3661c45bb26d078c085ad
Ilvan Lopes Jardim <62081883+Ilvan-J@users.noreply.github.com>
2025-03-22 01:44:17 -0300

    refactor: updating error responses.

--- media-collection/src/main/java/com/ilvan/media_collection/services/MediaService.java
+++ media-collection/src/main/java/com/ilvan/media_collection/services/MediaService.java
@@ -48,16 +48,16 @@ public class MediaService {
         var media = modelMapper.map(dto, Media.class);
 
         var typeMedia = typeMediaRepository.findById(dto.idTypeMedia())
-                .orElseThrow(() -> new CustomGenericException("Type media not found", HttpStatus.NOT_FOUND));
+                .orElseThrow(() -> new CustomGenericException("Type media not found!", HttpStatus.NOT_FOUND));
 
         var productionStatus = productionStatusRepository.findById(dto.idProductionStatus())
-                .orElseThrow(() -> new CustomGenericException("Production status not found", HttpStatus.NOT_FOUND));
+                .orElseThrow(() -> new CustomGenericException("Production status not found!", HttpStatus.NOT_FOUND));
 
         var watchingStatus = watchingStatusRepository.findById(dto.idWatchingStatus())
-                .orElseThrow(() -> new CustomGenericException("Watching status not found", HttpStatus.NOT_FOUND));
+                .orElseThrow(() -> new CustomGenericException("Watching status not found!", HttpStatus.NOT_FOUND));
 
         var user = userRepository.findById(UUID.fromString(token.getName()))
-                .orElseThrow(() -> new CustomGenericException("User not found", HttpStatus.NOT_FOUND));
+                .orElseThrow(() -> new CustomGenericException("User not found!", HttpStatus.NOT_FOUND));
 
         media.setTypeMedia(typeMedia);
         media.setProductionStatus(productionStatus);
@@ -82,27 +82,27 @@ public class MediaService {
         var userId = UUID.fromString(token.getName());
 
         var user = userRepository.findById(userId)
-                .orElseThrow(() -> new CustomGenericException("User not found", HttpStatus.NOT_FOUND));
+                .orElseThrow(() -> new CustomGenericException("User not found!", HttpStatus.NOT_FOUND));
 
         TypeMedia typeMedia = null;
 
         if (idTypeMedia != null) {
             typeMedia = typeMediaRepository.findById(idTypeMedia)
-                    .orElseThrow(() -> new CustomGenericException("Type media not found", HttpStatus.NOT_FOUND));
+                    .orElseThrow(() -> new CustomGenericException("Type media not found!", HttpStatus.NOT_FOUND));
         }
 
         ProductionStatus productionStatus = null;
 
         if (idProductionStatus != null) {
             productionStatus = productionStatusRepository.findById(idProductionStatus)
-                    .orElseThrow(() -> new CustomGenericException("Production Status not found", HttpStatus.NOT_FOUND));
+                    .orElseThrow(() -> new CustomGenericException("Production Status not found!", HttpStatus.NOT_FOUND));
         }
 
         WatchingStatus watchingStatus = null;
 
         if (idWatchingStatus != null) {
             watchingStatus = watchingStatusRepository.findById(idWatchingStatus)
-                    .orElseThrow(() -> new CustomGenericException("Watchin status not found", HttpStatus.NOT_FOUND));
+                    .orElseThrow(() -> new CustomGenericException("Watchin status not found!", HttpStatus.NOT_FOUND));
         }
 
         var orderByProperty = "dateOfAdded";
@@ -137,7 +137,7 @@ public class MediaService {
         var userIdReceived = UUID.fromString(token.getToken().getSubject());
 
         if (!media.getUser().getUserId().equals(userIdReceived)) {
-            throw new CustomGenericException("You are not authorized to read ", HttpStatus.FORBIDDEN);
+            throw new CustomGenericException("Unauthorized operation!", HttpStatus.FORBIDDEN);
         }
 
         return new MediaResponseDto(media.getIdMedia(),
@@ -157,22 +157,22 @@ public class MediaService {
         var media = modelMapper.map(dto, Media.class);
 
         media = mediaRepository.findById(idMedia)
-                .orElseThrow(() -> new CustomGenericException("Media not found", HttpStatus.NOT_FOUND));
+                .orElseThrow(() -> new CustomGenericException("Media not found!", HttpStatus.NOT_FOUND));
 
         var userIdReceived = UUID.fromString(token.getToken().getSubject());
 
         if (!media.getUser().getUserId().equals(userIdReceived)) {
-            throw new CustomGenericException("You are not authorized to update this media", HttpStatus.FORBIDDEN);
+            throw new CustomGenericException("Unauthorized operation!", HttpStatus.FORBIDDEN);
         }
 
         var typeMedia = typeMediaRepository.findById(dto.idTypeMedia())
-                .orElseThrow(() -> new CustomGenericException("Type media not found", HttpStatus.NOT_FOUND));
+                .orElseThrow(() -> new CustomGenericException("Type media not found!", HttpStatus.NOT_FOUND));
 
         var productionStatus = productionStatusRepository.findById(dto.idProductionStatus())
-                .orElseThrow(() -> new CustomGenericException("Production status not found", HttpStatus.NOT_FOUND));
+                .orElseThrow(() -> new CustomGenericException("Production status not found!", HttpStatus.NOT_FOUND));
 
         var watchingStatus = watchingStatusRepository.findById(dto.idWatchingStatus())
-                .orElseThrow(() -> new CustomGenericException("Watching status not found", HttpStatus.NOT_FOUND));
+                .orElseThrow(() -> new CustomGenericException("Watching status not found!", HttpStatus.NOT_FOUND));
 
         media.setIdMedia(idMedia);
         media.setName(dto.name());
@@ -188,10 +188,10 @@ public class MediaService {
     public void deleteMedia(UUID id, JwtAuthenticationToken token) {
         var userId = UUID.fromString(token.getToken().getSubject());
         var media = mediaRepository.findById(id)
-                .orElseThrow(() -> new CustomGenericException("Media not found", HttpStatus.NOT_FOUND));
+                .orElseThrow(() -> new CustomGenericException("Media not found!", HttpStatus.NOT_FOUND));
 
         if (!media.getUser().getUserId().equals(userId)) {
-            throw new CustomGenericException("You are not authorized to delete this media", HttpStatus.FORBIDDEN);
+            throw new CustomGenericException("Unauthorized operation!", HttpStatus.FORBIDDEN);
         }
 
         mediaRepository.delete(media);
