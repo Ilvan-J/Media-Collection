d4730ed7442103c043c543421323a6edab737773
Ilvan <ilvanlopesjardim110@gmail.com>
2025-05-02 11:24:30 -0300

    feat: added ProductionStatusResponseDto

--- media-collection/src/main/java/com/ilvan/media_collection/controller/dto/response/ProductionResponseDto.java
+++
@@ -1,4 +0,0 @@
-package com.ilvan.media_collection.controller.dto.response;
-
-public record ProductionResponseDto(Long id, String name) {
-}
---
+++ media-collection/src/main/java/com/ilvan/media_collection/controller/dto/response/ProductionStatusResponseDto.java
@@ -0,0 +1,4 @@
+package com.ilvan.media_collection.controller.dto.response;
+
+public record ProductionStatusResponseDto(Long id, String name) {
+}
